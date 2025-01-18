54610316f3aac883aeed0c84f2e2f0e7fc0dca38
aya446 <aya.w4474@gmail.com>
2025-01-18 09:02:06 +0200

    create add note state

--- lib/cubits/cubit/add_note_cubit_cubit.dart
+++ lib/cubits/cubit/add_note_cubit_cubit.dart
@@ -1,8 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part 'add_note_cubit_state.dart';
+part 'add_note_state.dart';
 
-class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
-  AddNoteCubitCubit() : super(AddNoteCubitInitial());
+class AddNoteCubitCubit extends Cubit<AddNoteState> {
+  AddNoteCubitCubit() : super(AddNoteInitial());
 }
