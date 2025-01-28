8c48a36efdf8a7828fccc3b98ac174b5762ad370
aya446 <aya.w4474@gmail.com>
2025-01-28 14:35:24 +0200

    finish colors list view

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final int color ;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 32,
-      backgroundColor: Colors.blue,
-    );
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: isActive
+            ? CircleAvatar(
+                radius: 32,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(radius: 29, backgroundColor: Color(color)),
+              )
+            : CircleAvatar(
+                radius: 29, backgroundColor: Color(color)));
   }
 }
 
+class ColorsListview extends StatefulWidget {
+  const ColorsListview({
+    super.key,
+  });
 
+  @override
+  State<ColorsListview> createState() => _ColorsListviewState();
+}
 
-class ColorsListview extends StatelessWidget {
-  const ColorsListview({super.key});
-
+class _ColorsListviewState extends State<ColorsListview> {
+  int currentIndex = 0;
+  List<int> colorsList =[ 0xFFff7092 , 0xFFff8e8a , 0xFFffb8a8 , 0xFFffcdb8 , 0xffffeedb ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 32*2 ,
+      height: 32 * 2,
       child: ListView.builder(
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return GestureDetector(
+            onTap: () {
+              currentIndex= index;
+              setState(() {
+                
+              });
+            },
+              child: ColorItem(
+              color: colorsList[index],
+              isActive: currentIndex == index ? true : false,
+          ),);
         },
-        itemCount: 10,
+        itemCount: colorsList.length,
         scrollDirection: Axis.horizontal,
-        
-        ),
+      ),
     );
   }
 }
